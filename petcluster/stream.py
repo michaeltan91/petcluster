941f4a7a99b2802f437d19a3cb7e4e42ea426849
Michael Tan <m.d.tan@tudelft.nl>
2021-07-22 13:54:29 +0200

    add mass fraction correction

--- petcluster/stream.py
+++ petcluster/stream.py
@@ -8,21 +8,29 @@ class Stream(object):
         # Collect the mass fractions of the stream of all the components in the simulation
         self.massfrac = stream.massfrac
 
-        #temp = [comp for comp, value in stream.massfrac.items() if value < 0.0001 ]
+        pop_list = [comp for comp, value in stream.massfrac.items() if value < 0.0001 ]
         # Dispose components with a fraction smaller than 0.0001 
-        #for element in temp:
-        #    self.massfrac.pop(element)
+        for element in pop_list:
+            self.massfrac.pop(element)
+        mass_total = sum(self.massfrac.values())
+        for comp, value in self.massfrac.items():
+            self.massfrac[comp] = value/mass_total
         
         # Collect the mole fractions of the stream of the all the components in the simulation 
         self.molefrac = stream.molefrac
 
-        #temp = [comp for comp, value in stream.molefrac.items() if value < 0.0001 ]
+        pop_list = [comp for comp, value in stream.molefrac.items() if value < 0.0001 ]
         # Dispose components with a fraction smaller than 0.0001
-        #for element in temp:
-        #    self.molefrac.pop(element)
+        for element in pop_list:
+            self.molefrac.pop(element)
+        mole_total = sum(self.molefrac.values())
+        for comp, value in self.molefrac.items():
+            self.molefrac[comp] = value/mole_total
 
         # Collect the mass flowrate, mole flowrate, temperature and pressure of the stream from the aspen simulation
         self.massflow = stream.massflow
         self.moleflow = stream.moleflow
+        self.volflow = stream.volflow
         self.pressure = stream.pressure
         self.temperature = stream.temperature
+        self.carbonfrac = 0
