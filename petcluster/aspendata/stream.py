9c897422fffb2d0156832abab8719957649b9836
Michael Tan <m.d.tan@tudelft.nl>
2022-04-11 10:31:39 +0200

    disable recalc of stream composition

--- petcluster/aspendata/stream.py
+++ petcluster/aspendata/stream.py
@@ -1,24 +1,25 @@
-
+'''Contains the data structure and retrieval for the stream data from the Aspen Plus simulation'''
 
 
 class Stream(object):
-
+    '''The main stream class for retrieving Aspen Plus stream data'''
     def __init__(self, stream):
 
         # Collect the mass fractions of the stream of all the components in the simulation
         self.massfrac = stream.massfrac
 
+        """
         pop_list = [comp for comp, value in stream.massfrac.items() if value < 0.0001 ]
-        # Dispose components with a fraction smaller than 0.0001 
+        # Dispose components with a fraction smaller than 0.0001
         for element in pop_list:
             self.massfrac.pop(element)
         mass_total = sum(self.massfrac.values())
         for comp, value in self.massfrac.items():
             self.massfrac[comp] = value/mass_total
-        
-        # Collect the mole fractions of the stream of the all the components in the simulation 
+        """
+        # Collect the mole fractions of the stream of the all the components in the simulation
         self.molefrac = stream.molefrac
-
+        """
         pop_list = [comp for comp, value in stream.molefrac.items() if value < 0.0001 ]
         # Dispose components with a fraction smaller than 0.0001
         for element in pop_list:
@@ -26,8 +27,9 @@ class Stream(object):
         mole_total = sum(self.molefrac.values())
         for comp, value in self.molefrac.items():
             self.molefrac[comp] = value/mole_total
-
-        # Collect the mass flowrate, mole flowrate, temperature and pressure of the stream from the aspen simulation
+        """
+        # Collect the mass flowrate, mole flowrate, temperature and pressure of the stream
+        # from the aspen simulation
         self.massflow = stream.massflow
         self.moleflow = stream.moleflow
         self.volflow = stream.volflow
@@ -36,9 +38,10 @@ class Stream(object):
         self.carbonfrac = 0
 
 
-    def calc_carbon_frac(self, component_list):
-        
+    def calc_carbon_frac(self, component_dict):
+        '''Calculate the carbon fraction on the stream by mass'''
         temp = 0
         for component, value in self.molefrac.items():
-            temp += self.moleflow * value * component_list['Carbon Atoms'][component] * 12.01 *8000*1E-6
+            temp += self.moleflow * value * component_dict['Carbon Atoms'][component] * \
+            12.01100000 *8000*1E-6
         self.carbonfrac = temp/self.massflow
