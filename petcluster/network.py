8b4ab5f02cfe1ad794b94622a4841483a1425c26
Michael Tan <m.d.tan@tudelft.nl>
2023-02-14 10:17:48 +0100

    add adj matrix vis

---
+++ petcluster/network.py
@@ -0,0 +1,68 @@
+import networkx as nx
+import matplotlib as plt
+import plotly.graph_objects as go
+
+class Network(object):
+
+    def __init__(self, multiplex, nodes, process_nodes):
+
+        self.multiplex=multiplex
+        self.nodes = nodes
+        self.process_nodes = process_nodes
+
+
+    def adjacency_matrix(self, weight, layer=None):
+
+        if layer is None:
+            node_list = sorted([x for x in self.multiplex.core_network.nodes if \
+                x[0] in self.process_nodes.keys()])
+
+            return (nx.to_numpy_array(self.multiplex.core_network,nodelist=node_list), 
+                [str(x) for x in node_list])
+
+        else:
+            labels, networks, _node_dict = self.multiplex.get_layers()
+            try:
+                labels.index(layer)
+                node_list = sorted([x for x in networks[labels.index(layer)].nodes if \
+                    x[0] in self.process_nodes.keys()])
+
+                return (nx.to_numpy_array(networks[labels.index(layer)], nodelist= node_list, weight=weight), \
+                    [x[0] for x in node_list])
+
+            except KeyError:
+                print(f'Layer {layer} is defined in the model')
+
+    
+    def visualize_adjacency_matrix(self, weighted = False, layer=None, **kwargs):
+
+        weight_options = {
+            "Material":"carbon_flow_rate",
+            "Steam": "energy",
+            "Electricity": "energy"
+        }
+        if weighted is True:
+            if layer is None:
+                raise KeyError
+            else:
+                weight = weight_options[layer]
+        else:
+            weight = 1
+
+
+        numeric_network, node_list = self.adjacency_matrix(weight, layer)
+        fig = go.Figure(data=go.Heatmap(
+            z=numeric_network,
+            x=node_list,
+            y=node_list,
+            xgap =1,
+            ygap=1,
+            colorscale = 'balance'
+        ))
+        fig.update_xaxes(side="top", tickangle=-90)
+        fig.update_yaxes(autorange="reversed")
+        fig.update_layout(**kwargs)
+        fig.show()
+
+
+
