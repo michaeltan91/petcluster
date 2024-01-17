15d7dfa33c9b59598653a74d9be87a73a9680fdb
michaeltan91 <56509550+michaeltan91@users.noreply.github.com>
2024-01-17 15:59:44 +0100

    change license

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='petcluster',
       version='1.0',
       description='TU Delft ESS E&I, Petrochemical cluster of Port of Rotterdam',
       author='Michael Tan',
-      license='Apache Licence 2.0',
-      packages=['petcluster'],
+      license='BSD 3-Clause License',
+      packages=find_packages(),
       install_requires = [
           'xlrd',
           'pyexcel',
